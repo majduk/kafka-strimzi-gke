271c84f95494d8e639b6f71b378406b97ea5c9ab
Michał Ajduk <majduk@google.com>
2024-02-12 17:17:23 +0100

    Update generate_provider.py

--- gcp-project/generate_provider.py
+++ gcp-project/generate_provider.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
 
-import hcl
 from jinja2 import Template
 import glob
 
-with open("terraform.tfvars", "r") as tfvars_in:
-      tfvars = hcl.load(tfvars_in)
+tfvars = {}
+try:
+  import hcl
+  for tfname in glob.glob("./*.tfvars"):
+    with open(tfname, "r") as tfvars_in:
+      tfvars = tfvars | hcl.load(tfvars_in)
+except ImportError:
+  sys.stderr.write('*tfvars config not supported. Install hcl module\n')
+try:
+  import json
+  for tfname in glob.glob("./*.tfvars.json"):
+    with open(tfname, "r") as tfvars_in:
+      tfvars = tfvars | json.load(tfvars_in)
+except ImportError:
+  sys.stderr.write('*tfvars.json config not supported. Install json module\n')
 
-for tfname in glob.glob("./*.j2"):
-    with open(tfname, "r") as f:
-        fname = tfname.replace(".j2","")
-        Template(f.read()).stream(tfvars).dump(fname)
+if len(tfvars) > 0:
+  for tfname in glob.glob("./*.j2"):
+      with open(tfname, "r") as f:
+          fname = tfname.replace(".j2","")
+          Template(f.read()).stream(tfvars).dump(fname)
+  exit(0)
+else:
+  sys.stderr.write("Configuration empty, aborting\n")
+  exit(1)
